6c88b3b06ed4b81dcdbe28457c65088e03bcfc70
Matras <martinholubek1@gmail.com>
2022-04-03 16:49:06 +0200

    Just Spring

--- src/main/java/com/lankaris/spring/basics/SpringIn5StepsApplicationBasic.java
+++ src/main/java/com/lankaris/spring/basics/SpringIn5StepsApplicationBasic.java
@@ -3,9 +3,11 @@ package com.lankaris.spring.basics;
 import com.lankaris.spring.basics.springin5steps.basic.BinarySearchImpl;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan
 public class SpringIn5StepsApplicationBasic {
 
 	public static void main(String[] args) {
@@ -14,7 +16,7 @@ public class SpringIn5StepsApplicationBasic {
 
 		//BinarySearchImpl binarySearch = new BinarySearchImpl(new BubbleSortAlgorithm());
 
-		ApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(SpringIn5StepsApplicationBasic.class);
 		BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
 		int result = binarySearch.binarySearch(new int[]{1,2,3}, 5);
 		System.out.println(binarySearch);
